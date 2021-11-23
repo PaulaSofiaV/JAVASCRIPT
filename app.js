ebf5b970ef8cf88cb5a915639449cc1f81f63b24
Paula Villarroel <paula_s_villarroel@hotmail.com>
2021-11-23 18:21:27 -0300

    ultima correccion

--- app.js
+++ app.js
@@ -1,83 +1,17 @@
 $("body").prepend('<p class="animacion" style="display: none" >No dudes en comunicarte!</p>');
 $("body").prepend('<p class="animacion2" style="display: none" >Redes al final de la pagina ♥</p>');
-$("p").show();
+$("p").fadeIn();
 
-$(".animacion").css({
-    "font-family": "Roboto Mono, monospace",
-    "color": "white"
-})
-
-
-
-
-
-/*
-let input1  = document.getElementById("botonEnviar");
-let input2  = document.getElementById("botonCancelar");
-input1.onkeyup   = () => {console.log("keyUp")};
-input2.onkeydown = () => {console.log("keyDown")};
-input1.addEventListener("click", (e) => {
-    e.preventDefault()
-    console.log(input.value)
-})
-
-input2.addEventListener("click", (e) => {
-    e.preventDefault()
-    console.log(input.value)
-})
-
-
-console.log($("h1"));
-console.log($("h2"));
-console.log($("h3"));
-
-console.log("-----------------------------")
-
-
-document.getElementById("botonEnviar").addEventListener("click", () => {
-    console.log(document.getElementById("precio").value);
-})
-
-document.getElementById("botonEnviar").addEventListener("click", () => {
-    console.log(document.getElementById("cantidad").value);
-})
-
-
-$(document).ready(function iniciando() {
-    console.log("El DOM esta listo")
-    
-})
-
-window.addEventListener('load', function() {
-    console.log( 'Todos los elementos de la ventana están cargados' );
-});
-
-
-
-$("body").prepend('<p class="animacion" style="display: none" >No dudes en comunicarte!</p>');
-$("body").prepend('<p class="animacion2" style="display: none" >Redes al final de la pagina ♥</p>');
-
-$(".animacion").fadeIn("slow",function(){
-    $(".animacion").fadeIn(1000);
+$(".animacion2").fadeIn("fast",function(){
+    $(".animacion2").fadeIn(800);
     });
 
-$(".animacion2").fadeIn("slow",function(){
-     $(".animacion2").fadeIn(1000);
-     });
-
 $(".animacion").css({
-    "font-family": "Roboto Mono, monospace",
-    "color": "white"
+   "font-family": "Roboto Mono, monospace",
+   "color": "white"
 })
 
 $(".animacion2").css({
-    "font-family": "Roboto Mono, monospace",
-    "color": "white"
+   "font-family": "Roboto Mono, monospace",
+   "color": "white"
 })
-
-$("h1").css("color", "white")
-        .slideUp(2000)
-        .delay(2000)
-        .slideDown(2000);
-
-*/
